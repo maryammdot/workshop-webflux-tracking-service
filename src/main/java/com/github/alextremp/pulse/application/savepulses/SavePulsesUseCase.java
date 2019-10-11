f2f1a9981d8090b085dad54b27d810320abbee1e
maryam.malek <maryam.malek@adevinta.com>
2019-10-11 08:34:19 +0200

    first steps

--- src/main/java/com/github/alextremp/pulse/application/savepulses/SavePulsesUseCase.java
+++ src/main/java/com/github/alextremp/pulse/application/savepulses/SavePulsesUseCase.java
@@ -1,12 +1,31 @@
 package com.github.alextremp.pulse.application.savepulses;
 
+import com.github.alextremp.pulse.application.savepulses.io.PulseEvent;
 import com.github.alextremp.pulse.application.savepulses.io.SavePulsesRequest;
 import com.github.alextremp.pulse.application.savepulses.io.SavePulsesResponse;
+import com.github.alextremp.pulse.domain.pulse.Pulse;
 import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 public class SavePulsesUseCase {
 
   public Mono<SavePulsesResponse> savePulses(SavePulsesRequest request) {
-    return Mono.error(new UnsupportedOperationException("Not implemented: SavePulsesUseCase"));
+    try {
+      createPulses(request);
+      return Mono.just(SavePulsesResponse.processed());
+    } catch (Throwable error) {
+      return Mono.just(SavePulsesResponse.error(error));
+    }
+  }
+
+  private void createPulses(SavePulsesRequest request) {
+    String id = UUID.randomUUID().toString();
+    String clientId = request.getId();
+    int i;
+    for(i = 0; i< request.getEvents().length; i++) {
+      PulseEvent pulseEvent = request.getEvents()[i];
+      new Pulse(id, clientId, pulseEvent.getName(), pulseEvent.getPayload());
+    }
   }
 }
